c03e0d21fb099a2b319c23341f504d0ceeca87ec
Aileen Cacayorin <acacayorin@gmail.com>
2015-10-23 14:23:56 -0700

    allow parent authentication by email or username

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,4 +1,6 @@
 class Parent < ActiveRecord::Base
+  attr_accessor :login
+
   has_one :bank
 
   validates :first_name, :presence => true
@@ -7,5 +9,15 @@ class Parent < ActiveRecord::Base
 
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+
+   def self.find_first_by_auth_conditions(warden_conditions)
+     conditions = warden_conditions.dup
+     if login = conditions.delete(:login)
+       where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+     else
+       where(conditions).first
+     end
+   end
 end
