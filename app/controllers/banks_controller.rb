3342dc32398340e9f8d234d73b27d332822ad2be
Aileen Cacayorin <acacayorin@gmail.com>
2015-10-23 19:33:46 -0700

    redirect to show bank page after bank creation

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,5 +1,37 @@
 class BanksController < ApplicationController
+  before_action :authenticate_parent!
+  before_action :set_default
+
   def new
+
     @bank = Bank.new
   end
+
+  def create
+    @bank = Bank.new(bank_params)
+    @bank.parent = @parent
+    if @bank.save
+      flash[:notice] = "Your bank was created!"
+      redirect_to parent_bank_path(@parent, @bank)
+    else
+      flash[:notice] = "Something went wrong."
+      redirect_to :back
+    end
+  end
+
+  def show
+    @bank = Bank.find(params[:id])
+  end
+
+
+  private
+
+  def set_default
+    @parent = Parent.find(params[:parent_id])
+  end
+
+  def bank_params
+    params.require(:bank).permit(:name)
+  end
+
 end
