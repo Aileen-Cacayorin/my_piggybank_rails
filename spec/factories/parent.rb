e0a7de57324252b9d7e95db597fe9b84ece6c48c
Aileen Cacayorin <acacayorin@gmail.com>
2015-11-01 16:14:53 -0800

    passing add a bank integration spec

--- spec/factories/parent.rb
+++ spec/factories/parent.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :parent do
     email 'test@example.com'
     password 'f4k3p455w0rd'
-    password_confirmation 'f4k3p455w0rd'
     username 'fakeuser'
     first_name "fake"
     last_name "user"
--- spec/features/add_bank_spec.rb
+++ spec/features/add_bank_spec.rb
@@ -3,13 +3,22 @@ require 'rails_helper'
 
 describe "the add a bank process" do
   it 'adds a bank to a parent' do
-    visit '/'
-    click_link "Parent Sign In"
-    parent = FactoryGirl.create(:parent)
-    login_as(parent, :scope => :parent)
-    save_and_open_page
+    sign_up
     fill_in 'Name', with: "My Bank"
     click_button "Add Bank"
     expect(page).to have_content "My Bank"
   end
+end
+
+
+def sign_up
+  visit "/"
+  click_link "Parent Sign up"
+  fill_in "Email", with: "testperson@gmail.com"
+  fill_in "Username", with: "testperson"
+  fill_in "First name", with: "test"
+  fill_in "Last name", with: "person"
+  fill_in "Password", with: "testperson"
+  fill_in "Password confirmation", with: "testperson"
+  click_button "Sign up"
 end
