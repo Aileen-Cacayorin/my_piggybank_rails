59566c2f11a1579506db79fb5aa524d6ac782b29
Aileen Cacayorin <acacayorin@gmail.com>
2015-12-01 13:49:22 -0800

    write deposit allowance method

--- app/models/allowance.rb
+++ app/models/allowance.rb
@@ -2,4 +2,20 @@ class Allowance < ActiveRecord::Base
   belongs_to :account
 
   validates :amount, :presence => true
+
+
+  def self.deposit_allowance
+    Allowance.all.each do |allowance|
+      account = allowance.account
+      amount = allowance.amount
+      transaction_type = "Deposit"
+      description = "allowance"
+      bank_id = account.child.bank.id.to_i
+
+      @transaction = Transaction.create(:description => description, :transaction_type => transaction_type, :amount => amount, :bank_id => bank_id)
+
+      @transaction.account = account
+      @transaction.save
+    end
+  end
 end
--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,3 +1,7 @@
 require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler::singleton
+
+scheduler.every '10m' do
+  Allowance.deposit_allowance
+end
